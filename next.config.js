a899843ae32bba2c46683d924dac8b54e4bdf149
Gabriel Antonio <mouragabriel205@gmail.com>
2023-06-08 15:33:21 -0400

    first configuration

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const path = require('path')
 
-module.exports = nextConfig
+module.exports = {
+  reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+  pageExtensions: ['page.tsx', 'page.ts', 'api.tsx', 'api.ts'],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+        port: '',
+        pathname: '**'
+      },
+    ],
+  },
+}
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,12 @@
+import { Button } from "react-bootstrap";
+import { Container } from "./styles";
+
+export default function Home() {
+  return (
+
+    <Container>
+      teste
+    </Container>
+
+  )
+}
